82ad0ed591120654afd5b649362593e2ce19765c
ruoqingsun <sunruoqing@gmail.com>
2015-12-02 11:49:27 +0100

    Modification on gameplay and condition panel
    1. Adjust the health bar of enemies
    2. Set the pump attacking range according to the distance to turbine
    3. Randomly generate wind direction (update every 5 seconds)
    4. Show instant wind turbine output according to the current wind
    direction (default direction of wind turbine is North)

---
+++ WindTurbine/Assets/Scripts/UICondition/OutputManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OutputManager : MonoBehaviour {
+
+	private int output;
+
+	// Use this for initialization
+	void Start () {
+		output = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		output = GameObject.FindGameObjectWithTag ("turbine").transform.GetComponent<TurbineInfo> ().output;
+		gameObject.transform.GetChild(0).GetComponent<Text>().text = "   Output: " + output + " km/s";
+	}
+}
---
+++ WindTurbine/Assets/Scripts/UICondition/WindManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WindManager : MonoBehaviour {
+
+	public string[] windDirection = {"N", "NE", "E", "SE", "S", "SW", "W", "NW"};
+	public int windIndex = 0;
+	public float windChangeTimeBetween = 5f;
+
+	private float timer;
+
+
+	// Use this for initialization
+	void Start () {
+		ChangeWind ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		timer += Time.deltaTime;
+		if (timer >= windChangeTimeBetween) {
+			ChangeWind();
+			timer -= windChangeTimeBetween;
+		}
+	
+	}
+
+	void ChangeWind(){
+
+		windIndex = Random.Range(0, 8);
+		Debug.Log ("WindDirection: " + windIndex);
+
+		gameObject.transform.GetChild(0).GetComponent<Text>().text = "  Wind: " + windDirection[windIndex];
+	
+	}
+
+}

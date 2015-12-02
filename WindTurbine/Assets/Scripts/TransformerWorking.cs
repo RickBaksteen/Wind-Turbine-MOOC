48b027e4570f406eb43edf2e8edf90992bcb46d1
ruoqingsun <sunruoqing@gmail.com>
2015-12-02 01:09:47 +0100

    Modifications
    Add Transformers into the game. Use lines to represent the connection.
    
    Change the attacking of the pump. It will attack the first enemy then
    the one with least health value.
    
    Add Info to each game element

---
+++ WindTurbine/Assets/Scripts/TransformerWorking.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TransformerWorking : MonoBehaviour {
+
+	public Transform pump;
+	public List<Transform> turbineLinks = new List<Transform>();
+
+	public bool enabled;
+
+	// Use this for initialization
+	void Start () {
+
+		enabled = false;
+
+		pump = GameObject.FindGameObjectWithTag("waterTower").transform;
+		//linkToWaterTower (pump);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (turbineLinks.Count <= 0)
+			enabled = false;
+
+	}
+
+	public void linkToWaterTower(Transform target)
+	{
+
+		if (enabled && target.CompareTag("waterTower")) {
+
+			GameObject newGameObject = new GameObject();
+			newGameObject.transform.SetParent(gameObject.transform);
+			newGameObject.transform.localPosition = Vector3.zero;
+			
+			LineRenderer lineRenderer = newGameObject.AddComponent<LineRenderer>();
+			lineRenderer.SetWidth(0.5f, 0.5f);
+			lineRenderer.SetPosition(0, newGameObject.transform.position);
+			lineRenderer.SetPosition(1, target.position);
+			
+			target.GetChild(1).GetComponent<PumpAttacking>().enableWork();
+			
+		}
+	}
+
+	public void linkToWaterTower()
+	{
+
+		pump = GameObject.FindGameObjectWithTag("waterTower").transform;
+
+		if (enabled && pump.CompareTag("waterTower")) {
+			
+			GameObject newGameObject = new GameObject();
+			newGameObject.transform.SetParent(gameObject.transform);
+			newGameObject.transform.localPosition = Vector3.zero;
+			
+			LineRenderer lineRenderer = newGameObject.AddComponent<LineRenderer>();
+			lineRenderer.SetWidth(0.5f, 0.5f);
+			lineRenderer.SetPosition(0, newGameObject.transform.position);
+			lineRenderer.SetPosition(1, pump.position);
+			
+			pump.GetChild(1).GetComponent<PumpAttacking>().enableWork();
+			
+		}
+	}
+
+	public void linkTurbine(Transform target){
+
+		if (target.CompareTag("turbine")) {
+
+			enabled = true;
+			turbineLinks.Add(target);
+
+		}
+
+	}
+
+	public void unlinkTurbine(Transform target){
+
+		if (target.CompareTag("turbine")) {
+
+			turbineLinks.Remove(target);
+			
+		}
+
+	}
+
+}
---
+++ WindTurbine/Assets/Scripts/TurbineInfo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurbineInfo : MonoBehaviour {
+
+	public int output = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

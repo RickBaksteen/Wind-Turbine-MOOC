407816457028cc42c2a81cec16df78a29f7ece85
ruoqingsun <sunruoqing@gmail.com>
2015-12-18 15:34:33 +0100

    Christmas present from Santa Panos and Santa Ruoqing
    1. New Score System at end of each level

---
+++ WindTurbine/Assets/Scripts/EndGameManager/ScoreShowing.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreShowing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.GetChild (0).GetComponent<Text> ().text = "Score: " + ScoreManager.score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

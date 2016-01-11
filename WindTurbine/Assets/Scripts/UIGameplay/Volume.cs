dfbd0323ac524c19c31cf9952bc041a42b4f5e50
ruoqingsun <sunruoqing@gmail.com>
2016-01-11 12:21:55 +0100

    Bug fixed with no reason
    Missing button bug fixed without any reason. I don’t know why they
    occurred and why they are well now

---
+++ WindTurbine/Assets/Scripts/UIGameplay/Volume.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Volume : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void volumeUp(){
+
+		AudioListener.volume = 0f ;
+
+	}
+
+	public void volumenDown(){
+
+
+
+	}
+
+	public void mute(){
+
+		AudioListener.volume = 0f;
+
+	}
+
+	public void unmute(){
+
+		AudioListener.volume = 1f;
+
+	}
+}
